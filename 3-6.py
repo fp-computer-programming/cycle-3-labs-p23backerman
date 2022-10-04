67129c95a3ddd463b3546421a657c618f8b52dc9
p23backerman <p23backerman@fairfieldprep.org>
2022-10-04 08:48:23 -0400

    3-6 (real)

---
+++ 3-6.py
@@ -0,0 +1,14 @@
+#var_string= 'Fairfield'
+#var_two='Prep'
+#print(var_string + ' ' + var_two)
+#var_string= '-'
+#print (var_string * 20)
+
+
+var_string = 'I\'m loving this short story I\'m reading, "The Fall of the House of Usher'
+print(var_string)
+a = 'I\'m loving this short story I\'m reading, "The Fall of the House of Usher'
+print(len(a))
+
+d = "apple"
+print(d[3])
