62061fc2e893b1f286a84f11a10ce2039ed19874
p23backerman <p23backerman@fairfieldprep.org>
2022-10-04 08:46:34 -0400

    3-5

---
+++ 3-5.py
@@ -0,0 +1,11 @@
+import time
+import math
+a= math.pow(2, 2)
+b= 2**2
+c= time.process_time()
+print(a, b)
+print(c)
+#before a = .058891
+#after a = .060019
+#after b = .036016
+#after print =.026938
